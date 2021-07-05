d34c2ca595f1459e147e9c619bb3f40c66f6ca54
Gregory Gruzdenkov <grzdnkv@gmail.com>
2021-07-05 12:10:19 +0000

    Update main

--- src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
+++ src/main/java/com/epam/rd/autotasks/meetstrangers/HelloStrangers.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class HelloStrangers {
     public static void main(String[] args) throws IOException {
         //Write a program, asks for a number - amount of strangers to meet.
-        //Then reads stranger names line by line and prints line by line "Hello, ".
+        //Then reads stranger names line by line and prints line by line "Hello, ...".
     }
 }
